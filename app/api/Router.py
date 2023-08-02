6c62cd1b9dae021ba424ad89c2e4894646d2e2ae
DELL <jogging1005@gmail.com>
2023-08-02 16:05:50 +0500

    JWTAuth

--- app/api/Router.py
+++ app/api/Router.py
@@ -1,52 +1,36 @@
-from fastapi import APIRouter,UploadFile
+from fastapi import APIRouter,UploadFile,Depends
 from fastapi.responses import RedirectResponse
-from app.api.log.logger import Log,levels
-import csv
-import codecs
-from app.api.database.db import Database
-import datetime
-from app.core.Config import settings,AdminData
-logger = Log(settings.logKey)
+from app.api.auth.auth_bearer import JWTBearer
+from app.core.Config import AdminLoginSchema
+from app.api.controller.csvHandler import processCsvFile
+from app.api.controller.login import AuthenticateCredentials
+from app.api.controller.verify import VerifyProduct
+
 router = APIRouter()
-database =Database(settings.dbPassword,settings.dbDatabase)
 
 @router.get("/")
 def redirect():
     return RedirectResponse("/docs")
 
-@router.get("/verify/{productID}")
-def verify(productID:str):
-    response = database.read({"VerificationCode":productID})
-    if response==None:
-        return {"detail":False}
-    else:
-        return {"detail":True}
+@router.get("/verify/{productId}")
+async def verify(productId:str):
+    try:
+
+        return VerifyProduct(productId)
+    except Exception as e:
+        return e
 
 
-@router.post("/addproducts/")
-async def AddProducts(LogInData:AdminData,file:UploadFile):
-    state,query = database.checkAdmin(LogInData.Username, LogInData.Password)
-    if not state:
-        if query==1:
-            logger.log(levels.warning,"Add Product Verification:Admin username does not exist")
-        elif query==2:
-            logger.log(levels.warning,f"Add Product Verification:Password for username '{LogInData.Username}' is incorrect")
-        return {"detail":query}
-    csvReader =csv.DictReader(codecs.iterdecode(file.file,'utf-8'))
-    IdsAdded=[]
-    for rows in csvReader:
-        Id = rows['VerificationCode']
-        if database.read({"VerificationCode":Id}) !=None:
-            continue
+@router.post("/login/")
+async def login(data:AdminLoginSchema):
+    try:
+        return AuthenticateCredentials(data)
+    except Exception as e:
+        return e
+@router.post("/addproducts/",dependencies=[Depends(JWTBearer())])
+async def AddProducts(file:UploadFile):
+    try:
+        return processCsvFile(file)
+    except Exception as e:
+        return e
 
-        IdsAdded.append(rows["VerificationCode"])
-
-        date = datetime.datetime.now()
-        date = date.strftime("%d/%m/%Y")
-        rows["DateAdded"] = date
-        database.writeRecords(rows)
-
-    if len(IdsAdded)!=0:
-        logger.log(levels.warning,f"Admin '{LogInData.Username}' added {len(IdsAdded)} records")
-        return "added records for",IdsAdded
-    return "No new records detected"
--- app/core/Config.py
+++ app/core/Config.py
@@ -1,11 +1,13 @@
 from pydantic_settings import BaseSettings,SettingsConfigDict
 from pydantic import BaseModel
+
 class Settings(BaseSettings):
     model_config = SettingsConfigDict(env_file=".env", env_file_encoding='utf-8')
-    dbPassword:str
+    dbUrl:str
     dbDatabase:str
     logKey:str
-class AdminData(BaseModel):
+
+class AdminLoginSchema(BaseModel):
     Username:str
     Password:str
 
